b552d3528833faae078c432d2d75d144ae641f2a
Sarvesh B Gugale <sarveshgugale7@gmail.com.com>
2026-05-07 17:59:04 +0530

    Fixing ENE

--- app.py
+++ app.py
@@ -11,5 +11,7 @@ def hello_world():
 @app.route('/health')
 def health():
     return 'Server is up and running'
-    
-app.run(debug=True, host='0.0.0.0', port=80)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=80)
